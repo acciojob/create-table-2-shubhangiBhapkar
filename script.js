63963bc7d19841263a86d165c7e24f84df3cd509
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-07-14 23:06:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 function createTable() {
     //Write your code here
-  
+	 const rn = prompt("Input number of rows");
+	 const cn = prompt("Input number of columns");
+
+	 const table = document.getElementById("myTable");
+	
+	if(cn <= 0 || rn <=0 || isNan(rn) || isNan(cn)){
+		return;
+	}
+
+	table.innerHTML ="";
+	
+	for(let i=1;i<=rn;i++){
+		const row = document.createElement("tr");
+		
+		
+		for(let j=1;j<=cn;j++){
+			const cell = document.createElement("td");
+			cell.textContent = `Row-${i} Column-${j}`;
+			row.appendChild(cell);
+		}
+		table.appendChild(cell);
+	}
 }
